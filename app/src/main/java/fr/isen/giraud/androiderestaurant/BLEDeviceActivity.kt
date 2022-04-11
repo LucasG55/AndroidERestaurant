feced21b4b79df6f4731eec3add502e049616802
LucasG55 <lucas.giraud@isen.yncrea.fr>
2022-04-11 13:23:50 +0200

    finalisation

--- app/src/main/java/fr/isen/giraud/androiderestaurant/BLEDeviceActivity.kt
+++ app/src/main/java/fr/isen/giraud/androiderestaurant/BLEDeviceActivity.kt
@@ -2,6 +2,8 @@ package fr.isen.giraud.androiderestaurant
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,6 +12,7 @@ import fr.isen.giraud.androiderestaurant.databinding.ActivityBledeviceBinding
 class BLEDeviceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBledeviceBinding
+    private var bluetoothGatt: BluetoothGatt? = null
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +21,47 @@ class BLEDeviceActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val device = intent.getParcelableExtra<BluetoothDevice?>("Device")
-        Toast.makeText(this, device?.address, Toast.LENGTH_SHORT).show()
         binding.deviceName.text = device?.name ?: "Unknown Name"
         binding.deviceStatut.setImageResource(R.drawable.statut_disconnected)
 
+        connectToDevice(device)
 
     }
+
+    override fun onStop() {
+        super.onStop()
+        closeBluetoothGatt()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun closeBluetoothGatt() {
+        bluetoothGatt?.close()
+        bluetoothGatt=null
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun connectToDevice(device: BluetoothDevice?) {
+        this.bluetoothGatt= device?.connectGatt(this, true, gattCallback)
+        this.bluetoothGatt?.connect()
+    }
+
+    private val gattCallback = object : BluetoothGattCallback() {
+        @SuppressLint("MissingPermission")
+        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+            super.onConnectionStateChange(gatt, status, newState)
+            when (newState) {
+                BluetoothGatt.STATE_CONNECTED -> {
+                    gatt?.discoverServices()
+                    runOnUiThread { binding.deviceStatut.setImageResource(R.drawable.statut_connected) }
+                }
+                BluetoothGatt.STATE_CONNECTING -> {
+                    runOnUiThread { binding.deviceStatut.setImageResource(R.drawable.statut_connecting) }
+                }
+                else -> {
+                    runOnUiThread { binding.deviceStatut.setImageResource(R.drawable.statut_disconnected) }
+                }
+            }
+        }
+    }
+
 }
